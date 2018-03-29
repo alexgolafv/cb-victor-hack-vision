a952b005d3988f2a275a163baf6fc09f027ba717
Alex Gola <alex.gola@flyvictor.com>
2018-03-29 15:14:01 +0100

    asdasdasdsdasd

--- messages-interpreter.js
+++ messages-interpreter.js
@@ -1,6 +1,7 @@
 const when = require('when');
 module.exports = {
     processMessage: processMessage,
+
 };
 
 const usersCache = {};
@@ -15,12 +16,19 @@ function processMessage(event, sendAnotherMsg) {
     if (!usersCache[event.user]) usersCache[event.user] = {};
     cache = usersCache[event.user];
 
+    
 
-    if(cache.lastQstHelp === true) {
+    if(cache.wantFlight || (event.text.toLowerCase().search(/(want)/) !== -1 
+            && event.text.toLowerCase().search(/(flight)/) !== -1)) {
+
         cache.lastQstHelp = false;
+        cache.wantFlight = true; 
+
         setTimeout(() => {
-            sendAnotherMsg('/yesno Found it. This flight should be the best choice for you. Do you want to book it? LND -> MIL 2018-02-02', 3000)
-        })
+            sendAnotherMsg('Found it. :sunglasses: \nThis flight should be the best choice for you.\n\n '+cache.from+' -> '+cache.to+' Do you want to book it?')
+            cache.wantFlight = false; 
+        }, 3000)
+
         return createResponse(`Let me check ...`); 
     }
 
@@ -30,7 +38,10 @@ function processMessage(event, sendAnotherMsg) {
             isNameRequested: false, 
             lastQstHowAreYou: null,
             lastQstHelp: false,
-            isGood: true,
+            isGood: null,
+            wantFlight: false,
+            from: null, 
+            to: null, 
             hasLastTrip: event.user === 'U9XCZ33EU',
             name: (event.user === 'U9XCWJPJ4' || event.user === 'U9XCZ33EU') ? 'Neelima' : event.user === 'U9Y8JQ80M' ? 'Meg' : null,
             isLogged: false, 
@@ -44,20 +55,19 @@ function processMessage(event, sendAnotherMsg) {
         return createResponse(`Hello ${cache.name || 'there' }! How you doing?`);
     }
     if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(fine|great|good|not bad)/) !== -1 && event.text.toLowerCase().search(/(not happy|not good)/) === -1){
-        cache.lastQstHowAreYou === false;
+        cache.lastQstHowAreYou = false;
         cache.isGood = true;
         cache.lastQstHelp = !cache.hasLastTrip;
-        return createResponse('I\'m happy to hear it! ' + (cache.hasLastTrip ? 'How was you last flight with Victor?' : 'Can I help you in anyway?'));
+        return createResponse('I\'m happy to hear it! :smile: ' + (cache.hasLastTrip ? 'How was you last flight with Victor?' : 'Can I help you in anyway?'));
     }
     if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(bad|not happy|not good)/) !== -1){
-        cache.lastQstHowAreYou === false;
+        cache.lastQstHowAreYou = false;
         cache.isGood = false;
         cache.lastQstHelp = !cache.hasLastTrip;
         return createResponse('Oh, really?! ' + (cache.hasLastTrip ? 'Is it about your last flight with Victor?' : 'Can I help you in anyway?'));
     }
 
+    return createResponse('Sorry, I do not understand. ' + (cache.isGood === null ? 'How are you today?' : 'Can I help you in anyway?'));
 
 
-
-    return createResponse('Sorry, I do not understand.');
 };
