b3e8af3529f83c69a2867ec7e27bf8ddf86b81c9
Alex Gola <alex.gola@flyvictor.com>
2018-03-29 12:45:41 +0100

    asdasd

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const { RTMClient }  = require('@slack/client');
 const slack_oauth= 'xoxb-337441105504-URrHFrFXjJJwhmuLZWQRby97';
 const slack_token    = 'xoxp-339213669079-337438635616-338292547413-429725eb274a798decdaa3c9de435059';
 
-const rtm = new RTMClient(slack_token);
+const rtm = new RTMClient(slack_oauth);
 rtm.start();
 
-
+let channel;
 rtm.on('authenticated', (rtmStartData) => {
     for (const c of rtmStartData.channels) {
-        if (c.is_member && c.name ==='jamiestestchannel') { channel = c.id }
+        if (c.is_member && c.name ==='general') { channel = c.id }
     }
-    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
 rtm.on('message', function () {
-    rtm.sendMessage("Hello!", channel);
+    rtm.sendMessage("Hello!", conversationId)  .then((res) => {
+        // `res` contains information about the posted message
+        console.log('Message sent: ', res.ts);
+      })
+      .catch(console.error);
 });
