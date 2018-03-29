a3fa157ddffa75ebe82d9ba902f7aaf5e44ec79c
Alex Gola <alex.gola@flyvictor.com>
2018-03-29 11:05:06 +0100

    add bot librarry and hot reloading

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const Cosmic = require('cosmicjs')
+const BootBot = require('bootbot')
+require('dotenv').config()
+const chrono = require('chrono-node')
+var schedule = require('node-schedule')
+const EventEmitter = require('events').EventEmitter
+
+var config = {}
+
+const reminders = []
+
+const eventEmitter = new EventEmitter()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.send("hey there boi")
+})
+
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    return res.send(req.query['hub.challenge'])
+  }
+  res.send('wrong token')
+})
+
+app.listen(app.get('port'), function(){
+  console.log('Started on port', app.get('port'))
+})
+
+
+const bot = new BootBot({
+  accessToken: process.env.ACCESS_TOKEN,
+  verifyToken: process.env.VERIFY_TOKEN,
+  appSecret: process.env.APP_SECRET
+}); 
+
+bot.setGreetingText("Hello, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ")
+
+bot.setGetStartedButton((payload, chat) => {
+  if(config.bucket === undefined){
+    chat.say('Hello my name is Note Buddy and I can help you keep track of your thoughts')
+    chat.say("It seems like you have not setup your bucket settings yet. That has to be done before you can do anything else. Make sure to type 'setup'")
+  }
+  BotUserId = payload.sender.id
+});
