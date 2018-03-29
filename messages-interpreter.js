b1cdf71994be309ab13b70a3176b0592aa61af98
Alex Gola <alex.gola@flyvictor.com>
2018-03-29 14:25:06 +0100

    asdasdasdasd

--- messages-interpreter.js
+++ messages-interpreter.js
@@ -12,12 +12,37 @@ function createResponse(text){
 }
 
 function processMessage(event) {
+console.log('asdasdasd', event);
     if (!usersCache[event.user]) usersCache[event.user] = {
-        isNameRequested: false
+        isNameRequested: false, 
+        lastQstHowAreYou: null,
+        helpQuestionMake: false,
+        isGood: true,
+        hasLastTrip: event.user === 'U9XCZ33EU',
     };
+    cache = usersCache[event.user];
+
+
     if(event.text.toLowerCase().search(/(hello|goodmorning|hi|hey)/) !== -1){
+        cache.lastQstHowAreYou = true;
         return createResponse('Hello there! How you doing?');
     }
+
+
+    if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(fine|great|good|not bad)/) !== -1 && event.text.toLowerCase().search(/(not happy|not good)/) === -1){
+        cache.lastQstHowAreYou === false;
+        cache.isGood = true;
+        return createResponse('I\'m happy to hear it! ' + (cache.hasLastTrip ? 'How was you last flight with Victor?' : 'Can I help you in anyway?'));
+    }
+
+
+    if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(bad|not happy|not good)/) !== -1){
+        cache.lastQstHowAreYou === false;
+        cache.isGood = true;
+        return createResponse('Oh, really?! ' + (cache.hasLastTrip ? 'Is it about your last flight with Victor?' : 'Can I help you in anyway?'));
+    }
+
+
 
 
     return createResponse('Sorry, I do not understand.');
