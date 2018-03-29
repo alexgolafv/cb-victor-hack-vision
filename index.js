1a0f232256fc5066ebb08200b0d344e3f657fa32
Alex Gola <alex.gola@flyvictor.com>
2018-03-29 14:52:38 +0100

    asdasdasd

--- index.js
+++ index.js
@@ -12,10 +12,21 @@ rtm.on('authenticated', (rtmStartData) => {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
+
 rtm.on('message', function (event) {
-    return interpreter.processMessage(event).then((res) => {
-        return rtm.sendMessage(res.text, event.channel).then((res) => {
+    const sendMsg = (text) => {
+        return rtm.sendMessage(text, event.channel).then((res) => {
             console.log('Message sent: ', res.ts);
-        })
+        });
+    }
+
+    return interpreter.processMessage(event, sendMsg).then((res) => {
+        return sendMsg(res.text);
     }).catch(console.error);
 });
+
+rtm.on('reaction_added', (event) => {
+    // For structure of `event`, see https://api.slack.com/events/reaction_added
+    console.log(`User ${event.user} reacted with ${event.reaction}`);
+  });
+  
--- messages-interpreter.js
+++ messages-interpreter.js
@@ -11,34 +11,48 @@ function createResponse(text){
     });
 }
 
-function processMessage(event) {
-console.log('asdasdasd', event);
-    if (!usersCache[event.user]) usersCache[event.user] = {
-        isNameRequested: false, 
-        lastQstHowAreYou: null,
-        helpQuestionMake: false,
-        isGood: true,
-        hasLastTrip: event.user === 'U9XCZ33EU',
-    };
+function processMessage(event, sendAnotherMsg) {
+    if (!usersCache[event.user]) usersCache[event.user] = {};
     cache = usersCache[event.user];
 
 
-    if(event.text.toLowerCase().search(/(hello|goodmorning|hi|hey)/) !== -1){
-        cache.lastQstHowAreYou = true;
-        return createResponse('Hello there! How you doing?');
+    if(cache.lastQstHelp === true) {
+        cache.lastQstHelp = false;
+        setTimeout(() => {
+            sendAnotherMsg('/yesno Found it. This flight should be the best choice for you. Do you want to book it? LND -> MIL 2018-02-02', 3000)
+        })
+        return createResponse(`Let me check ...`); 
     }
 
 
+    if(event.text.toLowerCase().search(/(hello|goodmorning|hi|hey)/) !== -1){
+        usersCache[event.user] = {
+            isNameRequested: false, 
+            lastQstHowAreYou: null,
+            lastQstHelp: false,
+            isGood: true,
+            hasLastTrip: event.user === 'U9XCZ33EU',
+            name: (event.user === 'U9XCWJPJ4' || event.user === 'U9XCZ33EU') ? 'Neelima' : event.user === 'U9Y8JQ80M' ? 'Meg' : null,
+            isLogged: false, 
+        };
+
+        cache = usersCache[event.user] 
+        cache.lastQstHowAreYou = true;
+        cache.isLogged = cache.name !== null;
+
+        console.log(event.user);
+        return createResponse(`Hello ${cache.name || 'there' }! How you doing?`);
+    }
     if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(fine|great|good|not bad)/) !== -1 && event.text.toLowerCase().search(/(not happy|not good)/) === -1){
         cache.lastQstHowAreYou === false;
         cache.isGood = true;
+        cache.lastQstHelp = !cache.hasLastTrip;
         return createResponse('I\'m happy to hear it! ' + (cache.hasLastTrip ? 'How was you last flight with Victor?' : 'Can I help you in anyway?'));
     }
-
-
     if(cache.lastQstHowAreYou === true && event.text.toLowerCase().search(/(bad|not happy|not good)/) !== -1){
         cache.lastQstHowAreYou === false;
-        cache.isGood = true;
+        cache.isGood = false;
+        cache.lastQstHelp = !cache.hasLastTrip;
         return createResponse('Oh, really?! ' + (cache.hasLastTrip ? 'Is it about your last flight with Victor?' : 'Can I help you in anyway?'));
     }
 
